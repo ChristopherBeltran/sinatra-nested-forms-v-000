fd7c498c14e9b14e170ee0f9e965a6f1856c60ee
Christopher Beltran <christod.beltran@gmail.com>
2018-11-25 04:57:57 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -16,7 +16,7 @@ module FormsLab
     end 
     
     post '/pirates' do
-      @pirate = Pirate.new(params[:student])
+      @pirate = Pirate.new(params[:pirate])
  
   params[:student][:courses].each do |details|
     Course.new(details)
